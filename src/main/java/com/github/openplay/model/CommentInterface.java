208641b8d69a9a42c92fbef832473248f3fe2d09
tomaspoeta96 <tomaspoeta96@gmail.com>
2017-10-19 10:56:10 -0300

    Tomás Updated CommentClass

--- src/main/java/com/github/openplay/model/CommentInterface.java
+++ src/main/java/com/github/openplay/model/CommentInterface.java
@@ -2,9 +2,35 @@ package com.github.openplay.model;
 
 import java.util.Date;
 
+import com.github.openplay.model.impl.User;
+
 public interface CommentInterface {
 
-	public void createComment(int userIdFrom, int userIdTo, Date date, String comment);
+	public int getCommentId();
 
-	public String getComment(); 
+	public void setCommentId(int commentId);
+
+	public Date getDate();
+
+	public void setDate(Date date);
+
+	public String getComment();
+
+	public void setComment(String comment);
+
+	public Long getUsers_UserIdFrom();
+
+	public void setUsers_UserIdFrom(Long users_UserIdFrom);
+
+	public Long getUsers_UserIdTo();
+
+	public void setUsers_UserIdTo(Long users_UserIdTo);
+
+	public User getUserfrom();
+
+	public void setUserfrom(User userfrom);
+
+	public User getUserto();
+
+	public void setUserto(User userto); 
 }
