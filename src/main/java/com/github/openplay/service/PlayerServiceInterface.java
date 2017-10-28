de39f57ae9af8f851a7f8d71dee945d343a9cc37
elimv <31315134+elimv@users.noreply.github.com>
2017-10-28 10:20:47 -0300

    Grupo negro primera version

---
+++ src/main/java/com/github/openplay/service/PlayerServiceInterface.java
@@ -0,0 +1,7 @@
+package com.github.openplay.service;
+
+import com.github.openplay.model.CampaingsHasUsersInterface;
+
+public interface PlayerServiceInterface {
+	CampaingsHasUsersInterface save(CampaingsHasUsersInterface campaign);
+}
