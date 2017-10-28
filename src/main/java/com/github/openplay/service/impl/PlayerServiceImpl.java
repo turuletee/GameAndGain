13dc993a1e0f810e48a7cddbd3f23ac1fe089239
elimv <31315134+elimv@users.noreply.github.com>
2017-10-28 10:21:18 -0300

    Grupo negro primera version

---
+++ src/main/java/com/github/openplay/service/impl/PlayerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.github.openplay.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.openplay.model.CampaingsHasUsersInterface;
+import com.github.openplay.model.impl.CampaignsHasUsers;
+import com.github.openplay.repository.CampaignsHasUsersRepository;
+import com.github.openplay.service.PlayerServiceInterface;
+
+
+@Service("playerService")
+public class PlayerServiceImpl implements PlayerServiceInterface{
+
+	@Autowired
+	private CampaignsHasUsersRepository campaignsHasUsersRepository;
+	
+	@Transactional
+	public CampaingsHasUsersInterface save(CampaingsHasUsersInterface campaign) {
+		return campaignsHasUsersRepository.save((CampaignsHasUsers) campaign);
+	}
+	
+}
