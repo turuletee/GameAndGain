166babb84fdf23e3e16081c8b766e4a11e7375b8
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-10-29 21:58:03 -0300

    Grupo Azul - upgrade comments

--- src/main/java/com/github/openplay/service/impl/AdminServiceImpl.java
+++ src/main/java/com/github/openplay/service/impl/AdminServiceImpl.java
@@ -1,13 +1,15 @@
-
 package com.github.openplay.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.openplay.model.CommentInterface;
 import com.github.openplay.model.UserInterface;
+import com.github.openplay.model.impl.Comment;
 import com.github.openplay.model.impl.User;
 import com.github.openplay.repository.AdminRepository;
+import com.github.openplay.repository.CommentRepository;
 import com.github.openplay.service.AdminService;
 
 @Service("adminService")
@@ -15,10 +17,15 @@ public class AdminServiceImpl implements  AdminService {
 
 	@Autowired
 	private AdminRepository adminRepository;
+	private CommentRepository commentRepository;
 	
 	@Transactional
 	public UserInterface save(UserInterface user) {
 		return adminRepository.save((User)user);
+	}
+	
+	public CommentInterface saveComment(CommentInterface comment) {
+		return commentRepository.save((Comment)comment);
 	}
 
 	public boolean findByLogin(String userName, String password) {	
