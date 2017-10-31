f5eebd9a9ca806d186a994091eaf9c1fbeff0306
elimv <31315134+elimv@users.noreply.github.com>
2017-10-31 20:44:20 -0300

    Grupo negro (anda)
    Problema solucionado.

--- src/main/java/com/github/openplay/resource/PlayerResourceInterface.java
+++ src/main/java/com/github/openplay/resource/PlayerResourceInterface.java
@@ -1,5 +1,10 @@
 package com.github.openplay.resource;
 
+import java.text.ParseException;
+
+import javax.ws.rs.core.Response;
+
 public interface PlayerResourceInterface {
+	public Response registerCampaign(String idUser, String idCampaign) throws ParseException;
 
 }
