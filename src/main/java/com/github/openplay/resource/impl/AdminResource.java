a9d4f3933527a6b9c19777de187fe3a952655dd1
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-02 17:36:42 -0300

    grupo azul create comments funcionando

--- src/main/java/com/github/openplay/resource/impl/AdminResource.java
+++ src/main/java/com/github/openplay/resource/impl/AdminResource.java
@@ -115,19 +115,19 @@ public class AdminResource implements AdminResourceInterface {
 	}
 	
 	@GET
-	@Path("TemporalComment")
+	@Path("temporalComment")
 	@Produces(MediaType.TEXT_HTML)
-	public Response getComments() {
+	public Response createComment() {
 		return Response.ok(new Viewable("/TemporalComment")).build();
 	}
 	
 	@POST
-	@Path("TemporalComment")
+	@Path("temporalComment")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_HTML)
 	public Response createComment(
-			@FormParam("userIdFrom") String users_UserIdFrom,
-			@FormParam("userIdTo") String users_UserIdTo,
+			@FormParam("users_UserIdFrom") String users_UserIdFrom,
+			@FormParam("users_UserIdTo") String users_UserIdTo,
 			@FormParam("date") String date,
 			@FormParam("comment") String comment
 			)
@@ -139,28 +139,17 @@ public class AdminResource implements AdminResourceInterface {
 		}
 
 		Comment newComment = new Comment();
-		
-		newComment.setUsers_UserIdFrom(Integer.parseInt(users_UserIdFrom));
-		newComment.setUsers_UserIdTo(Integer.parseInt(users_UserIdTo));
+
+
+		newComment.setUsers_userIdFrom(Integer.parseInt(users_UserIdFrom));
+		newComment.setUsers_userIdTo(Integer.parseInt(users_UserIdTo));
 		newComment.setDate(new java.sql.Date(new SimpleDateFormat("MM/dd/yyyy").parse(date).getTime()));
 		newComment.setComment(comment);
 		
+		
 		adminService.saveComment(newComment);
-		return Response.ok().entity(new Viewable("/TemporalComment")).build();
+		return Response.ok().entity(new Viewable("/success")).build();
 	}
 
-	@Override
-	public Response createComment() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Response getComment(String users_UserIdFrom, String users_UserIdTo,
-			String date, String comment) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
 }
-
