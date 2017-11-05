d2c8838474d2b8dbf6ff8d554ed48d63cf6d5634
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-05 15:38:40 -0300

    Add files via upload

---
+++ src/main/java/com/github/openplay/service/impl/CommentServiceImpl.java
@@ -0,0 +1,35 @@
+package com.github.openplay.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.openplay.model.CommentInterface;
+import com.github.openplay.model.UserInterface;
+import com.github.openplay.model.impl.Comment;
+import com.github.openplay.model.impl.User;
+import com.github.openplay.repository.AdminRepository;
+import com.github.openplay.repository.CommentRepository;
+import com.github.openplay.service.AdminService;
+import com.github.openplay.service.CommentService;
+
+@Service("commentService")
+public class CommentServiceImpl implements  CommentService {
+
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	@Transactional
+	public CommentInterface saveComment(CommentInterface comment) {
+		return commentRepository.save((Comment)comment);
+	}
+	
+	@Transactional
+	public CommentInterface getComment(int users_UserIdTo) {
+		CommentInterface c = commentRepository.findUserComment(users_UserIdTo);
+		return c;
+	}
+
+}
