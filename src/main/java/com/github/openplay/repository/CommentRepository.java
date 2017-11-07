1602e169348e8a3d4499d6c21ab6ddf360dfed84
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-07 12:32:47 -0300

    servicio getcoment funcionando

--- src/main/java/com/github/openplay/repository/CommentRepository.java
+++ src/main/java/com/github/openplay/repository/CommentRepository.java
@@ -1,5 +1,7 @@
 package com.github.openplay.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +13,8 @@ import com.github.openplay.model.impl.Comment;
 @Repository("commentRepository")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-	@Query("select comments, fecha from comments c where c.users_UserIdTo = :users_userIdTo")
-	CommentInterface findUserComment(@Param("users_userIdTo") int users_userIdTo);
+	@Query("select c from Comment c where c.users_UserIdTo = :users_userIdTo")
+	List<Comment> findUserComment(@Param("users_userIdTo") int users_userIdTo);
 	
 }
-
 
