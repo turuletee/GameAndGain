ce7ff8a124392de8a60dae02bcfbf5a7e037a30e
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-09 16:58:14 -0300

    getRanking con badges

--- src/main/java/com/github/openplay/service/impl/RankingServiceImpl.java
+++ src/main/java/com/github/openplay/service/impl/RankingServiceImpl.java
@@ -9,8 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.github.openplay.model.UserInterface;
 import com.github.openplay.model.impl.CampaignsHasUsers;
 import com.github.openplay.model.impl.Comment;
+import com.github.openplay.model.impl.UsersReceivesBadges;
 import com.github.openplay.repository.AdminRepository;
 import com.github.openplay.repository.RankingRepository;
+import com.github.openplay.repository.UsersReceivesBadgesRepository;
 import com.github.openplay.service.RankingService;
 
 @Service("rankingService")
@@ -22,15 +24,28 @@ public class RankingServiceImpl implements RankingService {
 	@Autowired
 	private AdminRepository adminRepository;
 	
+	@Autowired
+	private UsersReceivesBadgesRepository usersReceivesBadgesRepository;
+	
 	@Transactional
 	public List<CampaignsHasUsers> getRanking(String name) {
 		
 		UserInterface user = adminRepository.findByName(name);
 		
-		//int id = user.getUserId();
-		
 		List<CampaignsHasUsers> scores = rankingRepository.findById(user);
 		return scores;
 	}
+	
+	@Transactional
+	public List<UsersReceivesBadges> getBadge(String name) {
+		
+		UserInterface user = adminRepository.findByName(name);
+		
+		int id = user.getUserId();
+		
+		List<UsersReceivesBadges> badges = usersReceivesBadgesRepository.findById(id);
+		return badges;
+	}
 
 }
+
