a45019d746472adc73f10ae99701fa63b6591d51
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-09 15:42:16 -0300

    getRanking funcionando

--- src/main/java/com/github/openplay/service/impl/AdminServiceImpl.java
+++ src/main/java/com/github/openplay/service/impl/AdminServiceImpl.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.github.openplay.model.CommentInterface;
 import com.github.openplay.model.UserInterface;
-import com.github.openplay.model.impl.Comment;
 import com.github.openplay.model.impl.User;
 import com.github.openplay.repository.AdminRepository;
-import com.github.openplay.repository.CommentRepository;
 import com.github.openplay.service.AdminService;
 
 @Service("adminService")
@@ -23,6 +20,11 @@ public class AdminServiceImpl implements  AdminService {
 	@Transactional
 	public UserInterface save(UserInterface user) {
 		return adminRepository.save((User)user);
+	}
+	
+	@Transactional
+	public UserInterface findByName(String name) {
+		return adminRepository.findByName(name);
 	}
 
 	public boolean findByLogin(String userName, String password) {	
@@ -46,3 +48,4 @@ public class AdminServiceImpl implements  AdminService {
 	}
 
 	}
+
