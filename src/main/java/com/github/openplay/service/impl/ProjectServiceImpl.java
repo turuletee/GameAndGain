e617c18d955717b74bba825d5d1c635ccd0c52ed
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-11 20:31:46 -0300

    service updateDonor funcioando

--- src/main/java/com/github/openplay/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/github/openplay/service/impl/ProjectServiceImpl.java
@@ -1,11 +1,19 @@
 package com.github.openplay.service.impl;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.openplay.model.DonationsInterface;
+import com.github.openplay.model.UserInterface;
+import com.github.openplay.model.impl.Donations;
 import com.github.openplay.model.impl.Project;
 import com.github.openplay.model.impl.ProjectStates;
+import com.github.openplay.model.impl.User;
+import com.github.openplay.repository.AdminRepository;
+import com.github.openplay.repository.DonationsRepository;
 import com.github.openplay.repository.ProjectRepository;
 import com.github.openplay.repository.ProjectStatesRepository;
 import com.github.openplay.service.ProjectService;
@@ -19,6 +27,12 @@ public class ProjectServiceImpl implements ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
 	
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	@Autowired
+	private DonationsRepository donationsRepository;
+	
 	@Transactional
 	public ProjectStates getState(String nameProject) {
 		
@@ -27,6 +41,24 @@ public class ProjectServiceImpl implements ProjectService {
 		
 		ProjectStates states = projectStatesRepository.findById(id);
 		return states;
+	}
+	
+	@Transactional
+	public Donations updateDonor(String nameProject, String name, Date date) {
+		
+		Project project = projectRepository.findByName(nameProject);
+		int idProject = project.getProjectId();
+		
+		UserInterface user = adminRepository.findByName(name);
+		int idUser = user.getUserId();
+		
+		DonationsInterface donor = new Donations();
+		donor.setProjects_ProjectId(idProject);
+		donor.setDate(date);
+		donor.setUsers_UserId(idUser);
+		
+		return donationsRepository.save((Donations)donor);
+		
 	}
 
 }
