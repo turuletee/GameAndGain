c9e481a8d2d617607c8b7016dac640cf8eb01d6c
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-11 20:29:48 -0300

    service updateDonor funcionando

--- src/main/java/com/github/openplay/resource/impl/ProjectResource.java
+++ src/main/java/com/github/openplay/resource/impl/ProjectResource.java
@@ -1,6 +1,8 @@
 package com.github.openplay.resource.impl;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -50,6 +52,26 @@ public class ProjectResource {
 		String state = states.getStatus();
 		
 		System.out.println("El estado de esta camapaña es: " + state);
+		
+		return Response.ok().entity(new Viewable("/success")).build();
+	}
+	
+	@POST
+	@Path("updateDonor")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public Response updateDonor(
+			@FormParam("project") String project,
+			@FormParam("name") String name,
+			@FormParam("date") String date)
+			throws ParseException {
+
+		if (project == null || name == null) {
+			return Response.status(Status.PRECONDITION_FAILED).build();
+		}
+		
+		Date fecha = new java.sql.Date(new SimpleDateFormat("MM/dd/yyyy").parse(date).getTime());
+		projectService.updateDonor(project, name, fecha);
 		
 		return Response.ok().entity(new Viewable("/success")).build();
 	}
