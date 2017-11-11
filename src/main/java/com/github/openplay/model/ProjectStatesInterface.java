8e0f38e988de5dce47f549ec3307acdefe66e0aa
Gabriela <31229836+GabCast@users.noreply.github.com>
2017-11-11 10:18:37 -0300

    ProjectStates clase

---
+++ src/main/java/com/github/openplay/model/ProjectStatesInterface.java
@@ -0,0 +1,13 @@
+package com.github.openplay.model;
+
+public interface ProjectStatesInterface {
+	
+	public int getProject_StateId();
+
+	public void setProject_StateId(int project_StateId);
+
+	public String getStatus();
+
+	public void setStatus(String status);
+
+}
